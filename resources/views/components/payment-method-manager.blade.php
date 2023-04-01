f42764107493a4cc6a3f9b584bad877433e38769
Faruk Nasir <frknasir@yahoo.com>
2023-04-01 16:14:24 +0100

    feat: indicator for default payment method

--- resources/views/components/payment-method-manager.blade.php
+++ resources/views/components/payment-method-manager.blade.php
@@ -75,6 +75,12 @@
                         </svg>
                         Make Default
                     </button>
+                    @else
+                    <a href="javascript:;" class="inline-flex items-center rounded-md bg-gray-50 px-3 py-2 text-sm font-semibold text-green-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                        </svg>
+                    </a>
                     @endif
                     <button x-data @click="$store.paymentMethod.confirmRemoval({{ $authorization->id }})" type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                         <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
