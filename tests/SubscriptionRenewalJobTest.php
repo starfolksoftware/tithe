6a9afd7faa410ef1d133b7ebe6598b768388587f
Faruk Nasir <frknasir@yahoo.com>
2023-04-10 21:54:35 +0100

    fix:

--- tests/SubscriptionRenewalJobTest.php
+++ tests/SubscriptionRenewalJobTest.php
@@ -9,18 +9,6 @@ use Tithe\Tithe;
 test('subscription due for renewal can be renewed', function () {
     Bus::fake();
 
-    Carbon::setTestNow(now());
-
-    $plan = Tithe::newPlanModel()::factory()->create();
-    $subscriber = Team::factory()->create();
-    $subscription = Tithe::newSubscriptionModel()::factory()
-        ->for($plan)
-        ->for($subscriber, 'subscriber')
-        ->create([
-            'expired_at' => now(),
-            'grace_days_ended_at' => now()->addDays(3),
-        ]);
-
     SubscriptionRenewalJob::dispatch();
 
     Bus::assertDispatched(SubscriptionRenewalJob::class);
