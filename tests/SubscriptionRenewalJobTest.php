fc9fe9c44fdabf030658f424d68c0037c1151c54
frknasir <frknasir@users.noreply.github.com>
2023-04-10 20:56:29 +0000

    Fix styling

--- tests/SubscriptionRenewalJobTest.php
+++ tests/SubscriptionRenewalJobTest.php
@@ -1,10 +1,7 @@
 <?php
 
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Bus;
 use Tithe\Jobs\SubscriptionRenewalJob;
-use Tithe\Tests\Mocks\Team;
-use Tithe\Tithe;
 
 test('subscription due for renewal can be renewed', function () {
     Bus::fake();
